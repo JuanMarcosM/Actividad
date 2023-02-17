cc976f0c2cf36ac5d53908db891b1cc11c336b78
JM <juanmarcosgalindo17@gmail.com>
2023-02-17 21:00:42 +0100

    Segundo Commit

--- src/Actividad/GitAct.java
+++ src/Actividad/GitAct.java
@@ -1,11 +1,14 @@
 package Actividad;
 
+import java.util.Scanner;
+
 public class GitAct {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
-
+		
+		sc.close();
 	}
-
 }
